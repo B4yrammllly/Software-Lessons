f6c64b60e4d53b9ce986053a1c09fd32833fed07
B4yrammllly <109522372+B4yrammllly@users.noreply.github.com>
2022-07-31 10:29:25 +0300

    Add files via upload

---
+++ dilöğren.py
@@ -0,0 +1,60 @@
+import os
+
+os.system("clear")
+
+print("Tool holder : 【B】【4】【y】【r】【a】【m】【l】【l】【l】【y】\n\n")
+print("*******************************************\n Öğrenmek istediğiniz dilin ismini yazın\n*******************************************\nPython\nC#\nC\n")      
+dil = input("LÜTFEN DİL İSMİ GİRİNİZ : ")
+if(dil == "Python"):
+	os.system("clear")
+	print("\033[31m[1]Pekiştirme\n[2]ÖĞRENİM\n \033[0m")
+	işlem = input("İŞLEM NO GİRİNİZ : ")
+	if(işlem == "1"):
+		print("https://youtu.be/hhTZuWhYSkQ")
+	elif(işlem == "2"):
+		print("""https://youtube.com/playlist?list=PLURN6mxdcwL960S-bRuf1F6K09yzNjgcn\nhttps://youtube.com/playlist?list=PLi1BmHvgBkxIYweLR52cRJnit4AEEugn4""")
+	else:
+	 	print("GEÇERSİZ İŞLEM❌❌❌")
+	
+	
+	
+	
+	
+	
+if(dil == "C"):
+	os.system("clear")
+	print("\033[31m[1]Pekiştirme\n[2]ÖĞRENİM\n \033[0m")
+	işlem = input("İŞLEM NO GİRİNİZ : ")
+	if(işlem == "1"):
+		print("https://youtu.be/vHNgy_QBwlo")
+	elif(işlem == "2"):
+		print("""https://youtube.com/playlist?list=PLKnjBHu2xXNP-E_TjR-g5Tslm6dW4UH_3\nhttps://youtubecomplaylist?listPLIHume2cwmHdFsJRo5oYG7yQ4NyUx43ql""")
+	else:
+		print("GEÇERSİZ İŞLEM❌❌❌")
+
+
+
+
+
+
+
+
+
+
+
+
+if(dil == "C#"):
+	os.system("clear")
+	print("\033[31m[1]Pekiştirme\n[2]ÖĞRENİM\n \033[0m")
+	işlem = input("İŞLEM NO GİRİNİZ : ")
+	if(işlem == "1"):
+		print("https://youtu.be/hhTZuWhYSkQ")
+	elif(işlem == "2"):
+		print("""https://youtube.com/playlist?list=PLURN6mxdcwL960S-bRuf1F6K09yzNjgcn
+
+https://youtube.com/playlist?list=PLi1BmHvgBkxIYweLR52cRJnit4AEEugn4""")
+	else:
+		print("GEÇERSİZ İŞLEM❌❌❌")
+
+
+
